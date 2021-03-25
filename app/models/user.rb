51f7898e532d828e9880df5367639903a6f59cf4
NtwaliHeritier <heritierntwali@gmail.com>
2021-03-25 03:43:21 +0200

    add content to seeds.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ApplicationRecord
   has_secure_password
+  validates :username, :password, :last_name, :first_name, presence: true
+  validates :password, length: {minimum: 5}
+  validates :username, uniqueness: true
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -4,4 +4,5 @@
 # Examples:
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+#   Character.create(name: 'Luke', movie: movies.first)U
+User.create(first_name: "Ntwali", last_name: "Heritier", username: "hatsor", password: "hatsor", admin: true)

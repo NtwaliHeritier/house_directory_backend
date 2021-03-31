eed16689aa9c0644648e7ca1971bdb5abb6f4d1e
NtwaliHeritier <heritierntwali@gmail.com>
2021-03-31 14:06:27 +0200

    add favorite model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
+  has_many :houses
+
   validates :username, :password, :last_name, :first_name, presence: true
   validates :password, length: {minimum: 5}
   validates :username, uniqueness: true
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_31_094014) do
+ActiveRecord::Schema.define(version: 2021_03_31_120134) do
+
+  create_table "favorites", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "house_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["house_id"], name: "index_favorites_on_house_id"
+    t.index ["user_id"], name: "index_favorites_on_user_id"
+  end
 
   create_table "houses", force: :cascade do |t|
     t.string "name"
@@ -32,5 +41,7 @@ ActiveRecord::Schema.define(version: 2021_03_31_094014) do
     t.boolean "admin", default: false
   end
 
+  add_foreign_key "favorites", "houses"
+  add_foreign_key "favorites", "users"
   add_foreign_key "houses", "users"
 end

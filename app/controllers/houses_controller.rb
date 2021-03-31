db4eb929d768980c0a0eb5e9e84ddd23e777f886
NtwaliHeritier <heritierntwali@gmail.com>
2021-03-31 14:26:30 +0200

    add create function to house and favorite controllers

--- app/controllers/houses_controller.rb
+++ app/controllers/houses_controller.rb
@@ -1,2 +1,18 @@
 class HousesController < ApplicationController
+  before_action :authorized
+
+  def create
+    @house = House.new(house_params)
+    @house.user_id = @user.id
+    if @house.save
+      render json: @house
+    else
+      render json: @house.errors, status: :unprocessable_entity
+    end
+  end
+
+  private 
+    def house_params
+      params.require(:house).permit!
+    end
 end
